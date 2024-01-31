dad7bb7d0d3025042e3afca4c70f45a66f782ad1
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2024-01-30 20:21:58 -0500

    Use timer for intake command temporarily

--- src/main/java/frc/robot/AutoCommands/IntakeCommand.java
+++ src/main/java/frc/robot/AutoCommands/IntakeCommand.java
@@ -1,22 +1,28 @@
 package frc.robot.AutoCommands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Platform.Subsystems;
 
 public class IntakeCommand extends Command {
+    Timer timer;
+
     @Override
     public void initialize() {
         Subsystems.intake.intakeNote();
+        timer = new Timer();
+        timer.start();
     }
 
     @Override
     public boolean isFinished() {
-        return Subsystems.intake.hasNote();
+        //return Subsystems.intake.hasNote();
+        return timer.get() > 3.0;
     }
 
     @Override
     public void end(boolean interrupted) {
-        if(!interrupted) return; //dont call endIntake because it ends itself
+        //if(!interrupted) return; //dont call endIntake because it ends itself
         Subsystems.intake.idleIntake();
     }
 }
