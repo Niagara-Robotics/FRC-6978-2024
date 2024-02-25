bbafdf7f8182f6434e53258a153f34c2ef8eac9e
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2024-02-25 02:05:27 -0500

    Switch lowerLauncher command to parameter-based control

--- src/main/java/frc/robot/AutoCommands/LowerLauncherCommand.java
+++ src/main/java/frc/robot/AutoCommands/LowerLauncherCommand.java
@@ -1,12 +1,18 @@
 package frc.robot.AutoCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Framework.ParameterHandle;
 import frc.robot.Platform.Subsystems;
 
 public class LowerLauncherCommand extends Command {
+    ParameterHandle<Double> positionHandle;
+
     @Override
     public void initialize() {
-        Subsystems.launcher.setTiltPosition(0);
+        positionHandle = Subsystems.launcher.tilt.getHandle("LowerCommand");
+        positionHandle.takeControl(false);
+        positionHandle.set(0.0);
+
     }
 
     @Override
