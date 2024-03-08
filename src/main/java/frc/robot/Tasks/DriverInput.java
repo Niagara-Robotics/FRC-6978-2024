7e1d4efe7e5120d8cb5990bc8874c9c82120e2da
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2024-03-07 20:04:40 -0500

    Update driver controls

--- src/main/java/frc/robot/Tasks/DriverInput.java
+++ src/main/java/frc/robot/Tasks/DriverInput.java
@@ -2,8 +2,16 @@ package frc.robot.Tasks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
+import org.opencv.core.Mat;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.SPI.Mode;
 import frc.robot.Framework.IPeriodicTask;
 import frc.robot.Framework.PIDController;
 import frc.robot.Framework.ParameterHandle;
@@ -15,14 +23,26 @@ import frc.robot.Platform.Subsystems;
 public class DriverInput implements IPeriodicTask {
     boolean guestActive = false;
     boolean autoAlign = false;
+    boolean autoNote = false;
 
-    PIDController alignmentController;
+    PIDController noteController;
 
     ParameterHandle<Double> tiltHandle;
+
+    SPI testPort;
+
+    long lastSendTS;
     
+    Timer animTimer;
+
     public DriverInput() {
         tiltHandle = Subsystems.launcher.tilt.getHandle("driver");
+        animTimer = new Timer();
+        
+        animTimer.start();
+        
     }
+
 
     void driveStickVelocity(double x, double y, double xMultiplier, double yMultiplier) {
         x = (Math.abs(x) > Constants.Drive.deadZone)? 
@@ -57,11 +77,19 @@ public class DriverInput implements IPeriodicTask {
         
 
         if(autoAlign) {
-                Subsystems.telemetry.pushDouble("autoAlign.delta", Subsystems.tracking.shotTargetX - 300);
-                Subsystems.telemetry.pushDouble("autoAlign.output", alignmentController.process(Subsystems.tracking.shotTargetX - 300));
+            //Subsystems.telemetry.pushDouble("autoAlign.delta", alignmentController. - 640);
+            //Subsystems.telemetry.pushDouble("autoAlign.output", alignmentController.process(Subsystems.tracking.odometry.getPoseMeters().getRotation().getRadians()));
 
-                Subsystems.differentialDrive.driveChassisSpeeds(new ChassisSpeeds(0,0, alignmentController.process(Subsystems.tracking.shotTargetX - 300)));
-        } else driveStickVelocity(x, y, 2, 3);
+        } else if(autoNote) {
+            Subsystems.telemetry.pushDouble("autoNote.delta", Subsystems.tracking.noteTargetX);
+            Subsystems.telemetry.pushDouble("autoNote.output", noteController.process(Subsystems.tracking.noteTargetX));
+
+            double output = noteController.process(Subsystems.tracking.noteTargetX);
+            if(Subsystems.tracking.noteTargetY > 1) {output = 0.0;};
+
+            Subsystems.differentialDrive.driveChassisSpeeds(new ChassisSpeeds(0.8,0, output));
+
+        } else driveStickVelocity(x, y, 3, 3.6);
 
         //shoot button
         if(Hardware.driverStick.getRawButtonPressed(Constants.OperatorControls.launcherButton)) {
@@ -96,7 +124,8 @@ public class DriverInput implements IPeriodicTask {
 
     public void onStart(RunContext ctx) {
         guestActive = false;
-        alignmentController = new PIDController(0.0075, 0.0, 0.5);
+        
+        noteController = new PIDController(0.020, 0.0, 0.5);
         if(!tiltHandle.hasControl()) tiltHandle.takeControl(false);
     }
 
@@ -115,13 +144,24 @@ public class DriverInput implements IPeriodicTask {
         }
 
         if(Hardware.driverStick.getRawButtonPressed(Constants.DriverControls.autoAlignButton)) {
-            alignmentController.init();
-            alignmentController.set(0);
-            alignmentController.setLimit(1.1);
+            
             autoAlign = true;
             Subsystems.autoShot.fullAutoLaunch();
         } else if(Hardware.driverStick.getRawButtonReleased(Constants.DriverControls.autoAlignButton)) {
             autoAlign = false;
+            Subsystems.differentialDrive.driveChassisSpeeds(new ChassisSpeeds(0,0,0));
+            Subsystems.autoShot.cancelAutoLaunch();
+        }
+
+        if(Hardware.driverStick.getRawButtonPressed(Constants.DriverControls.autoNoteButton)) {
+            noteController.init();
+            noteController.set(0.0);
+            noteController.setLimit(1.1);
+            autoNote = true;
+            Subsystems.intake.intakeNote();
+        } else if(Hardware.driverStick.getRawButtonReleased(Constants.DriverControls.autoNoteButton) || 
+            (Subsystems.intake.intakeFinished() &&autoNote)) {
+            autoNote = false;
             Subsystems.differentialDrive.driveChassisSpeeds(new ChassisSpeeds(0,0,0));
         }
         
@@ -131,12 +171,20 @@ public class DriverInput implements IPeriodicTask {
             useMainDriverStick();
         }
 
-        if(Hardware.driverStick.getRawButtonPressed(Constants.DriverControls.tiltTakeover)) 
-        tiltHandle.takeControl(false);
+        //tiltHandle.takeControl(false);
 
         if(Hardware.driverStick.getRawButtonPressed(1)) {
             tiltHandle.takeControl(false);
-            tiltHandle.set(0.0);
+            tiltHandle.set(-0.1);
+
+            if(RandomGenerator.getDefault().nextDouble() > 0.98) {
+                System.out.println("Parked tile mechanism");
+                System.out.println("*tilte");
+                System.out.println("*tilt");
+                System.out.println("damn autocorrect");
+            } else {
+                System.out.println("Parked tilt mechanism");
+            }
         } else if (Hardware.driverStick.getRawButtonPressed(4)) {
             tiltHandle.set(0.023);
         }
@@ -144,12 +192,39 @@ public class DriverInput implements IPeriodicTask {
         if(Hardware.driverStick.getRawButtonPressed(Constants.DriverControls.ampShotButton)) {
             Subsystems.autoShot.setupAmpShot();
         }
+
+        if(Hardware.driverStick.getRawButtonPressed(Constants.DriverControls.trapShotButton)) {
+            Subsystems.autoShot.setupTrapShot();
+        }
+
+        if(Hardware.driverStick.getRawButtonPressed(Constants.DriverControls.intakeButton)) {
+            Subsystems.intake.intakeNote();
+        } else if(Hardware.driverStick.getRawButtonReleased(Constants.DriverControls.intakeButton)) {
+            //idleIntake();
+        }
         
-        //new SPI(SPI.Port.kOnboardCS0).;
+        
     }
 
     public void publishTelemetry() {
         Subsystems.telemetry.pushBoolean("driverInput.hasTiltControl", tiltHandle.hasControl());
+        //Subsystems.telemetry.pushDouble("autoAlign.closestAngle", closestAngleDelta(Subsystems.autoShot.angleToTarget(), Subsystems.tracking.odometry.getPoseMeters().getRotation().getRadians()));
+        
+        if((System.nanoTime() - lastSendTS)> 25000000) {
+            lastSendTS = System.nanoTime();
+            byte data[] = new byte[32];
+
+            data[0] = (byte)0xf4;
+            data[1] = 0; //slot
+            data[2] = 1; //cmd: set len
+            data[3] = 10;
+            
+            //testPort.write(data, 32);*/
+            //Subsystems.illumination.setStatic((byte)0, (int)(50*((animTimer.get() % 1.0)/1.0)), 0, 0);
+            Subsystems.telemetry.pushDouble("illumRed", (int)(50*((animTimer.get() % 25.0)/25.0)));
+        }
+
+        //Subsystems.illumination.setStatic((byte)0, (int)(50*(animTimer.get() % 1.0)), 0, 0);
     }
 
     public void onStop() {
