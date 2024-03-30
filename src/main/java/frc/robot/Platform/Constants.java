8a2067678cdc5933d55e48014cb81066503388aa
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2024-03-30 14:18:18 -0400

    retune from waterloo

--- src/main/java/frc/robot/Platform/Constants.java
+++ src/main/java/frc/robot/Platform/Constants.java
@@ -8,7 +8,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class Constants {
     public static class Drive {
         //volts
-        public static double kP = 0.2;
+        public static double kP = 0.3;
         public static double kI = 0.0;
         public static double kD = 0.000;
         public static double kVright = 0.109;
@@ -34,13 +34,13 @@ public class Constants {
 
     public static class Tracking {
         public static double zClamp = 620;
-        public static double minCornerDist = 22;
+        public static double minCornerDist = 19;
     }
 
     public static class Launcher {
         public static double rotorToMeters = 5;
 
-        public static double maxWheelCurrent = 15;
+        public static double maxWheelCurrent = 35;
 
         //60,12
         //default stage 2 velocity target(this is also parameterized)
@@ -61,26 +61,24 @@ public class Constants {
 
         public static double tiltRotorToMechanismRatio = 50.0 / (2.0*Math.PI);
         //both of these need to be POSITIVE!!
-        public static double tiltMaxOutputUp = 3.0;
-        public static double tiltMaxOutputDown = 0.95;
-        public static double tiltKp = 75; //TODO: estimate for appropriate kP with new sensor ratio
-        public static double tiltKd = 0.6;
-        public static double tiltKg = 0.333; //TODO: determine appropriate kG for cosine approximation
+        public static double tiltMaxOutputUp = 3.2;
+        public static double tiltMaxOutputDown = 1.2;
+        public static double tiltKp = 85; //TODO: estimate for appropriate kP with new sensor ratio
+        public static double tiltKd = 2.0;
+        public static double tiltKg = 0.313; //TODO: determine appropriate kG for cosine approximation
 
         public static double tiltDefaultPosition = -0.1134464014;
 
-        public static double tiltMaxPosition = 1.19; //0.195rot TODO: determine max tilt position in radians
+        public static double tiltMaxPosition = 1.23; //0.195rot TODO: determine max tilt position in radians
         public static double tiltMinPosition = -0.113446;
 
-        public static double tiltTolerance = 0.0081;
+        public static double tiltTolerance = 0.0065;
 
-        public static double ampTiltPosition = 1.0;
+        public static double ampTiltPosition = 0.86;
 
-        public static double trapTiltPosition = 1.05;
+        public static double trapTiltPosition = 1.23;
 
         public static double trapVelocity = 70;
-
-        
     }
 
     public static class AutoShot {
@@ -94,9 +92,9 @@ public class Constants {
         public static HashMap<Double, Double> velocityMap = new HashMap<Double,Double>() { {
             put(1370.0, 60.0);
             put(1930.0, 70.0);
-            put(2280.0, 80.0);
+            put(2280.0, 70.0);
             put(2740.0, 80.0);
-            put(3220.0, 100.0);
+            put(3220.0, 90.0);
         }};
 
         //TODO: remap tilt position in radians
@@ -105,9 +103,9 @@ public class Constants {
             put(1930.0, 0.13*2*Math.PI);
             put(2280.0, 0.09*2*Math.PI);
             put(2740.0, 0.35);
-            put(3220.0, 0.029*2*Math.PI);
-            put(3600.0, 0.0);
-            put(4000.0, -0.1);
+            put(3220.0, 0.163362818);
+            put(3600.0, 0.05);
+            put(4000.0, -0.03);
         }};
 
         public static HashMap<Integer, Pose2d> redTrapFiringPoints = new HashMap<Integer, Pose2d>() {{
@@ -132,10 +130,12 @@ public class Constants {
         public static double trapWhiskerLength = 0.85;
 
         public static boolean dropTiltAfterSpeakerShot = true;
+
+        public static double spitVelocity = 15;
     }
 
     public static class Intake {
-        public static double floorRollerPower = 0.55;
+        public static double floorRollerPower = 0.65;
         public static double indexRollerVelocity = 7.5;
 
         public static double indexer_kV = 7.04 / 63;
@@ -146,7 +146,8 @@ public class Constants {
     }
 
     public static class Lift {
-        public static double power = 0.5;
+        public static double powerUp = 0.4;
+        public static double powerDown = 0.2;
         public static double kP = 1.2;
         public static double kF = 1.4;
         public static double velocity = 240;
@@ -154,14 +155,15 @@ public class Constants {
     }
 
     public static class AutoPilot {
-        public static double anglekP = 4.5;
+        public static double anglekP = 8.0;
         public static double angularVelocityLimit = 3.0; //radians per second
+        public static double angularVelocityThreshold = 0.05;
 
-        public static double distancekP = 4.0;
+        public static double distancekP = 3.5;
         public static double linearVelocityLimit = 1.5; //radians per second
-        public static double deltaStopPoint = 0.8;
+        public static double deltaStopPoint = 0.6;
 
-        public static double distanceThreshold = 0.04;
+        public static double distanceThreshold = 0.08;
     }
 
     public static class DriverControls {
